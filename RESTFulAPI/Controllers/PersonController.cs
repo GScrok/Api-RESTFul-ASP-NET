8974662794584ed46d0786d1cbaa520fdb89fb42
GScrok <grmscrok@gmail.com>
2024-07-22 14:52:05 -0300

    END Module 9 - Versionamento de Endpoints

--- RESTFulAPI/Controllers/PersonController.cs
+++ RESTFulAPI/Controllers/PersonController.cs
@@ -1,11 +1,13 @@
+using Asp.Versioning;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulAPI.Model;
 using RESTFulAPI.Services;
 
 namespace RESTFulAPI.Controllers
 {
+    [ApiVersion("1")]
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]/v{version:apiVersion}")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
